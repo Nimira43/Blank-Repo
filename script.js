1040b3b74178b8a88bb9d83ec72756477b547e9c
Nick Rabone <n.rabone72@aol.com>
2024-08-12 05:44:05 +0100

    Circles increase in size when scrolled.

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let circle2 = document.querySelector('.circle-2')
 
 window.addEventListener('scroll', () => {
   let scrollValue = window.scrollY
-  circle1.computedStyleMap.clipPath = `circle(${150 + scrollValue * 0.75})`
+  circle1.style.clipPath = `circle(${150 + scrollValue * 0.75}px at 0 0)`
+  circle2.style.clipPath = `circle(${150 + scrollValue * 0.75}px at 100% 100%)`
 })
